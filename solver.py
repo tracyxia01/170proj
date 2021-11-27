15cd0f28fdf06c045d90344238322a31d5c65829
Tracy Xia <tracyxia@berkeley.edu>
2021-11-26 20:15:51 -0800

    starter code

---
+++ solver.py
@@ -0,0 +1,20 @@
+from parse import read_input_file, write_output_file
+import os
+
+def solve(tasks):
+    """
+    Args:
+        tasks: list[Task], list of igloos to polish
+    Returns:
+        output: list of igloos in order of polishing  
+    """
+    pass
+
+
+# Here's an example of how to run your solver.
+# if __name__ == '__main__':
+#     for input_path in os.listdir('inputs/'):
+#         output_path = 'outputs/' + input_path[:-3] + '.out'
+#         tasks = read_input_file(input_path)
+#         output = solve(tasks)
+#         write_output_file(output_path, output)
