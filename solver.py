95f4e1f97f4ca58ec1cbe2d2f5d2e3e4b75beb50
Tracy Xia <tracyxia@berkeley.edu>
2021-11-30 21:08:25 -0800

    first attempt based on mays outline 11/30

--- solver.py
+++ solver.py
@@ -6,17 +6,48 @@ def solve(tasks):
     Args:
         tasks: list[Task], list of igloos to polish
     Returns:
-        output: list of igloos in order of polishing  
+        output: list of igloos in order of polishing
     """
     # first we have to sort the file
     tasks.sort(key = lambda x: x.get_max_benefit())
-    pass
+    timeslots = [0] * 1440
+    for task in tasks:
+        id = task.get_task_id()
+        deadline = task.get_deadline()
+        duration = task.get_duration()
+        found_slot = False
+        while not found_slot:
+            if deadline <= 0:
+                break
+            found_slot, time_copy = find_slot(deadline, timeslots, id, duration)
+            if found_slot:
+                timeslots = time_copy
+                break
+            deadline -= 1
+    sequence = set(timeslots)
+    sequence.discard(0)
+    #print(list(sequence))
+    return list(sequence)
+
+def find_slot(end_time, timeslots, id, duration):
+    time_copy = timeslots
+    for i in range(end_time-1, max(-1, end_time-duration-1), -1):
+        if timeslots[i] == 0:
+            time_copy[i] = id
+        else:
+            return False, timeslots
+    return True, time_copy
 
 
-# Here's an example of how to run your solver.
-# if __name__ == '__main__':
-#     for input_path in os.listdir('inputs/'):
-#         output_path = 'outputs/' + input_path[:-3] + '.out'
-#         tasks = read_input_file(input_path)
-#         output = solve(tasks)
-#         write_output_file(output_path, output)
+if __name__ == '__main__':
+    for input_path in os.listdir('inputs/'):
+        if input_path == '.DS_Store':
+            continue
+        for input_path2 in os.listdir('inputs/' + input_path):
+             #print(input_path2)
+             if input_path2 == '.ipynb_checkpoints' or input_path2 == '.DS_Store':
+                 continue
+             output_path = 'outputs/' + input_path + '/' + input_path2[:-3] + '.out'
+             tasks = read_input_file('inputs/' + input_path + '/' + input_path2[:-3] + '.in')
+             output = solve(tasks)
+             write_output_file(output_path, output)
