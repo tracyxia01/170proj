1ba0e7d09b6d0dac716d2fb5bb2e97fdfaa12bab
May Liu <chonghualiu@berkeley.edu>
2021-12-04 13:08:02 -0800

    added basic framework for the SA algorithm

--- solver.py
+++ solver.py
@@ -8,48 +8,88 @@ def solve(tasks):
     Returns:
         output: list of igloos in order of polishing
     """
-    # 1. find_base
-    base = find_base(tasks)
-    # 2. siumlated annealing on the base
+    base = basic_greedy(tasks) # We first find the bast 
+    res = simulated_annealing(base) # apply simulated annealing to the base array
+    return res
     
 
 
-def find_base(tasks):
+def basic_greedy(tasks):
+    """
+    Args:
+        tasks: list[Task], list of igloos to polish
+    Returns:
+        output: a basic solution obtained by greedy. Served as the starting point for SA. 
+    """
     # first we have to sort the file
     tasks.sort(key = lambda x: x.get_max_benefit())
-    timeslots = [0] * 1440 # so it ranges from 0 to 1439
+    timeslots = [0] * 1440
     for task in tasks:
         id = task.get_task_id()
         deadline = task.get_deadline()
         duration = task.get_duration()
-        find_time(deadline, timeslots, id, duration)
+        find_slot(deadline, timeslots, id, duration)
     sequence = set(timeslots)
     sequence.discard(0)
     #print(list(sequence))
     return list(sequence)
 
-# passes in the current task information
-# fill in the timeslot if we can find one
-# if not, don't do anything
-# Return nothing. So we distructively modify the timeslot. 
-def find_time(end_time, timeslots, id, duration):
-    # set up the start and end time
+def find_slot(end_time, timeslots, id, duration):
+    """
+    Args:
+        end_time: the end time of the current task
+        timeslots: the current order of tasks already assigned
+        id: the id of current task
+        duration: duration of the current task
+    Returns:
+        discrutively modify timeslots to put our current job in it. 
+        Doesn't return anything.  
+    """
     curr_end = end_time
-    curr_start = curr_end - duration + 1
+    curr_start = curr_end - duration
     found = False
-    # we want to look at every reasonable spot 
-    # We also have to take into consideration that 
-    # the next task starts at the same task the prev was finished
+
     while curr_start >= 0 and not found:
-        # so ignore the start and end, as long as the middle ones are all zero
-        if sum(timeslots[curr_start + 1 : curr_end]) == 0:
-            for i in range(curr_start, curr_end + 1):
-                # We have to check if the entire timeslot if 0
+        if sum(timeslots[curr_start : curr_end]) == 0:
+            for i in range(curr_start, curr_end):
                 timeslots[i] = id
-                found = True
+            found = True
         else:
+            # we shift and compare 
             curr_end -=  1
             curr_start -= 1
+
+
+def simulated_annealing(s):
+    """
+    Args: 
+        task_lst: the initial lst for SA
+    Returns:
+        output: the final task array with optimal values
+    """
+    t = 20 # should be large. But need further testing
+    while not freeze(s): 
+        s_prime = permute(s)
+        delta = cost(s_prime) - cost(s)
+        if delta < 0:
+            s = s_prime
+        else:
+            # TODO
+            # replace s = s_price with probability of e^(-delta/t)
+            pass
+    return 0
+
+def freeze(task_lst):
+    # TODO
+    pass
+
+def permute(task_lst):
+    # TODO
+    pass
+
+def cost(task_lst):
+    # TODO
+    pass
 
 if __name__ == '__main__':
     for input_path in os.listdir('inputs/'):
